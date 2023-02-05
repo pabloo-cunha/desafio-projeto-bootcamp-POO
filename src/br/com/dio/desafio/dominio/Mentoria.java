8c936d6945fa17236293197d4e9b709979c9812e
pabloo-cunha <pablo.limacunha@outlook.com>
2023-02-04 21:56:16 -0300

    Projeto concluido

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -24,10 +24,8 @@ public class Mentoria extends Conteudo{
 
     @Override
     public String toString() {
-        return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
-                '}';
+        return "\nMentoria: " + getTitulo() + '\'' +
+                "\ndescricao: " + getDescricao() + '\'' +
+                "\ndata: " + data;
     }
 }
